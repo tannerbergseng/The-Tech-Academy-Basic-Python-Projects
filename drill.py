aa923e6057d947b603175f807a5a9ffc1c570721
tannerbergseng <50608517+tannerbergseng@users.noreply.github.com>
2019-07-17 15:01:27 -0700

    Delete drill.py

--- drill.py
+++
@@ -1,8 +0,0 @@
-import os
-items = os.listdir("C:\\Users\\tanne\\Documents\\python_projects")
-
-newlist = []
-for names in items:
-    if names.endswith(".txt"):
-        newlist.append(names)
-print (newlist)
