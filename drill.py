f35eb6106ca69fed8ee88fc63bd6a0afa6a29f3b
tannerbergseng <50608517+tannerbergseng@users.noreply.github.com>
2019-07-22 18:14:36 -0700

    Add files via upload

---
+++ drill.py
@@ -0,0 +1,23 @@
+import os
+items = os.listdir("C:\\Users\\tanne\\Documents\\python_projects")
+
+
+
+newlist = []
+path = os.path.join
+for names in items:
+    if names.endswith(".txt"):
+        newlist.append(names)
+print (newlist)
+
+time = os.path.getmtime(items)
+
+
+for names in items:
+    if names.endswith(".txt"):
+       print(time) 
+
+
+
+
+
